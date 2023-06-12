3c059e2b4d4877826eab083af5f6002b5adc3380
rakib-molla <rakibmail1997@gmail.com>
2023-06-12 21:12:53 +0600

    new update

--- index.js
+++ index.js
@@ -243,20 +243,19 @@ async function run() {
 
 
     // create payment intent
-    app.post('/create-payment-intent', async (req, res) => {
+    app.post('/create-payment-intent/', async (req, res) => {
       const { price } = req.body;
       const amount = parseInt(price * 100);
-      console.log('price is', price);
-
+      console.log(price, amount);
       const paymentIntent = await stripe.paymentIntents.create({
         amount: amount,
-        currency: 'usd',
-        payment_method_types: ['card']
+        currency: "usd",
+        payment_method_type: ['card']
       });
-
+    
       res.send({
-        clientSecret: paymentIntent.client_secret
-      })
+        clientSecret: paymentIntent.client_secret,
+      });
     })
 
 
